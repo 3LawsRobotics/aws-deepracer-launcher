b6ca2fe7b3beb1bc2b6ccbd0afc0f7474cf97a05
Pierre Quebriac <pierre.quebriac@gmail.com>
2023-07-17 14:00:17 -0700

    remove unused dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,14 +8,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # cd async_web_server_cpp
 # git checkout ros2
 # apt-get install -y python3-websocket
-echo ""
-echo "Cloning the web_video_server package"
-cd $SCRIPT_DIR
-git clone https://github.com/RobotWebTools/web_video_server.git
-cd web_video_server
-git fetch origin pull/111/head:ros2
-git checkout ros2
-echo ""
-echo "Cloning the rplidar_ros package from 3laws fork"
-cd $SCRIPT_DIR
-git clone https://github.com/3LawsRobotics/rplidar_ros.git -b ros2
+# echo ""
+# echo "Cloning the web_video_server package"
+# cd $SCRIPT_DIR
+# git clone https://github.com/RobotWebTools/web_video_server.git
+# cd web_video_server
+# git fetch origin pull/111/head:ros2
+# git checkout ros2
+# echo ""
+# echo "Cloning the rplidar_ros package from 3laws fork"
+# cd $SCRIPT_DIR
+# git clone https://github.com/3LawsRobotics/rplidar_ros.git -b ros2
